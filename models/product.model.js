01a95baf8a02ae30079f5d3df53e023772d5469f
Ivan <68665931+Eir-Rithsen@users.noreply.github.com>
2025-07-31 21:11:05 -0300

    Create product.model.js

---
+++ models/product.model.js
@@ -0,0 +1,23 @@
+import { db } from '../config/firebase.js';
+
+const collection = db.collection('products');
+
+export const getAllProducts = async () => {
+  const snapshot = await collection.get();
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+};
+
+export const getProductById = async (id) => {
+  const doc = await collection.doc(id).get();
+  if (!doc.exists) return null;
+  return { id: doc.id, ...doc.data() };
+};
+
+export const createProduct = async (data) => {
+  const docRef = await collection.add(data);
+  return { id: docRef.id };
+};
+
+export const deleteProduct = async (id) => {
+  await collection.doc(id).delete();
+};
