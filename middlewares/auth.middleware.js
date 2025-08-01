642ec4e79d40281551c718c0a5934d4ea0f1fb1d
Ivan <68665931+Eir-Rithsen@users.noreply.github.com>
2025-07-31 21:10:21 -0300

    Create auth.middleware.js

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+export const authenticate = (req, res, next) => {
+  const header = req.headers['authorization'];
+  if (!header) return res.status(401).json({ error: 'Token requerido' });
+
+  const token = header.split(' ')[1];
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch {
+    res.status(403).json({ error: 'Token inválido' });
+  }
+};
